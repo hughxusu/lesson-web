f5f43466696a2feeb2769d11756263f54c2037eb
hughxusu <hughxusu@qq.com>
2025-10-11 22:19:43 +0800

    增加登陆演示页面

---
+++ codes/0-introduce/index.js
@@ -0,0 +1,14 @@
+let login = document.querySelector('.right .sign-in');
+login.addEventListener('click', function() {
+    alert('click login!');
+});
+
+let forgotPwd = document.querySelector('.right .forgot-pwd');
+forgotPwd.addEventListener('click', function() {
+    alert('click forgot password!');
+});
+
+let register = document.querySelector('.right .register');
+register.addEventListener('click', function() {
+    alert('click register!');
+});
